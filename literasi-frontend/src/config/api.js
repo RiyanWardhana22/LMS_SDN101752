470023314a53362a9e12ed866db810fae1688b6a
riyanwardhana <riyanwardhana2@gmail.com>
2026-08-05 19:18:51 +0700

    FIX: API.js

--- literasi-frontend/src/config/api.js
+++ literasi-frontend/src/config/api.js
@@ -1,13 +1,7 @@
-const ENV_URL = import.meta.env.VITE_API_BASE_URL;
-const FALLBACK_URL = "http://localhost/lms_sdn101752/literasi-backend";
-export const API_BASE_URL = ENV_URL || FALLBACK_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const apiEndpoint = (path) => {
   if (!path) return API_BASE_URL;
   const cleanPath = path.startsWith("/") ? path.slice(1) : path;
-  const cleanBase = API_BASE_URL.endsWith("/")
-    ? API_BASE_URL.slice(0, -1)
-    : API_BASE_URL;
-
-  return `${cleanBase}/${cleanPath}`;
+  return `${API_BASE_URL.endsWith("/") ? API_BASE_URL.slice(0, -1) : API_BASE_URL}/${cleanPath}`;
 };
